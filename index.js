26274bc475d91a89eefc279187388165d045fb5d
john argote <argote.john@gmail.com>
2018-06-23 22:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function destructivelyDeleteFromObjectByKey(object,key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object,key) {
-  var food = {fajita:'2 plates',pork:'3 pieces'};
-  delete food.pork;
-  return food;
+  delete object.key;
+  return object;
 }
