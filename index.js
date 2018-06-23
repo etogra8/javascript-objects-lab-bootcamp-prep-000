71a35cde81e346918480962f6b7f7ef2aec5f931
john argote <argote.john@gmail.com>
2018-06-23 21:38:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }  
 
 function deleteFromObjectByKey(object,key) {
-  
+  var obj={corn:'2 cups'};
+  var newObj=Object.assign({},obj);
+  delete newObj.corn;
+  return obj
 }
